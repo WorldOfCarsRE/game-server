d0161dbca069aebb4b46d308e4e520b6ba3caf99
Rocket <me@rocketprogrammer.me>
2023-03-19 20:35:35 -0500

    ai: Basic interactive object NPC shop test

--- ai/carplayer/InteractiveObjectAI.py
+++ ai/carplayer/InteractiveObjectAI.py
@@ -3,11 +3,17 @@ from .DistributedCarAvatarAI import DistributedCarAvatarAI
 TYPE_NPC = 0
 
 # Interation types
+CMD_TYPE_NEGATIVE = 0
+CMD_TYPE_POSITIVE = 1
+
 COMMAND_OFFER_QUERY_INTERACTIONS = 1
 COMMAND_OFFER_PLAYER_APPROACH = 54
 COMMAND_OFFER_PLAYER_RETREAT = 55
 COMMAND_OFFER_PLAYER_CLICK = 57
 COMMAND_CLICK = COMMAND_OFFER_PLAYER_CLICK
+
+COMMAND_OFFER_SHOP = 32
+COMMAND_OFFER_GAME = 33
 
 class InteractiveObjectAI(DistributedCarAvatarAI):
     def __init__(self, air):
@@ -42,5 +48,12 @@ class InteractiveObjectAI(DistributedCarAvatarAI):
     def getClientScript(self):
         return 'scripts/interactive/default_npc_no_physics.lua'
 
-    def triggerInteraction(self, queryType: int, args: list):
-        print(f'triggerInteraction - {queryType} - {args}')
+    def triggerInteraction(self, eventId: int, args: list):
+        avatarId = self.air.currentAvatarSender
+        print(f'triggerInteraction - {eventId} - {args}')
+
+        if eventId == COMMAND_CLICK:
+            self.d_setInteractiveCommands(avatarId, eventId, [COMMAND_OFFER_SHOP, 31012, CMD_TYPE_POSITIVE])
+
+    def d_setInteractiveCommands(self, avatarId: int, eventId: int, args: list):
+        self.sendUpdateToAvatar(avatarId, 'setInteractiveCommands', [eventId, [args]])
