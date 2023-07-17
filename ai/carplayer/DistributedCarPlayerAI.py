e57346916c352699a9e17d5a7b75a67c8430039e
Rocket <me@rocketprogrammer.me>
2023-07-16 21:10:35 -0500

    ai: Basic coin giving

--- ai/carplayer/DistributedCarPlayerAI.py
+++ ai/carplayer/DistributedCarPlayerAI.py
@@ -1,10 +1,12 @@
 from .DistributedCarAvatarAI import DistributedCarAvatarAI
+from ai import ZoneConstants
 
 class DistributedCarPlayerAI(DistributedCarAvatarAI):
     def __init__(self, air):
         DistributedCarAvatarAI.__init__(self, air)
         self.DISLname = ''
         self.DISLid = 0
+        self.carCoins = 0
 
     def setDISLname(self, DISLname):
         self.DISLname = DISLname
@@ -15,13 +17,31 @@ class DistributedCarPlayerAI(DistributedCarAvatarAI):
     def setDISLid(self, DISLid):
         self.DISLid = DISLid
 
+    def setCarCoins(self, carCoins: int):
+        self.carCoins = carCoins
+
+    def getCarCoins(self) -> int:
+        return self.carCoins
+
     def announceGenerate(self):
-        self.sendUpdateToAvatar(self.air.currentAvatarSender, 'setDISLname', [self.DISLname])
-        self.sendUpdateToAvatar(self.air.currentAvatarSender, 'setRuleStates', [[[100, 1, 1, 1]]]) # To skip the tutorial, remove me to go to tutorial.
-        self.sendUpdateToAvatar(self.air.currentAvatarSender, 'generateComplete', [])
+        self.sendUpdateToAvatar(self.doId, 'setDISLname', [self.DISLname])
+        self.sendUpdateToAvatar(self.doId, 'setRuleStates', [[[100, 1, 1, 1]]]) # To skip the tutorial, remove me to go to tutorial.
+        self.sendUpdateToAvatar(self.doId, 'generateComplete', [])
 
     def sendEventLog(self, event: str, params: list, args: list):
         self.air.writeServerEvent(event, self.doId, f'{params}:{args}')
 
     def persistRequest(self, context: int):
-        self.sendUpdateToAvatar(self.air.currentAvatarSender, 'persistResponse', [context, 1])
+        self.sendUpdateToAvatar(self.doId, 'persistResponse', [context, 1])
+
+    def invokeRuleRequest(self, eventId: int, rules: list, context: int):
+        print(f'invokeRuleRequest - {eventId} - {rules} - {context}')
+
+        if eventId in ZoneConstants.MINIGAMES:
+            level, score = rules
+
+            self.d_updateCoins(score)
+            self.sendUpdateToAvatar(self.doId, 'invokeRuleResponse', [eventId, rules, context])
+
+    def d_updateCoins(self, coins: int):
+        self.sendUpdateToAvatar(self.doId, 'setCarCoins', [coins])
