31f628b681a0e25382c3f2def76f634bb9f16801
Rocket <me@rocketprogrammer.me>
2024-08-07 06:14:16 -0500

    otp-startup: Add logging

--- startup/unix/start_all.sh
+++ startup/unix/start_all.sh
@@ -1,6 +1,6 @@
 cd ../../config
 
-screen -dmS OTP "../../OtpGo/otpgo" otp.yml
+screen -dmS OTP "../../OtpGo/otpgo" otp.yml >otpgo.log 2>&1
 
 cd ..
 screen -dmS Districts python3 -m DistrictStarter
