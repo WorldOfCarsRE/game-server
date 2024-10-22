c0b4e06394d4ac67592fca947b84222fdcec2cb1
Rocket <me@rocketprogrammer.me>
2024-10-22 09:56:58 -0500

    minigames: Implement `Luigi's Casa Della Tires`

--- game/cars/carplayer/games/LuigisCasaDellaTiresAI.py
+++ game/cars/carplayer/games/LuigisCasaDellaTiresAI.py
@@ -2,19 +2,19 @@ from game.cars.carplayer.InteractiveObjectAI import (
     COMMAND_OFFER_PLAYER_APPROACH, InteractiveObjectAI)
 
 
-class DocsClinicAI(InteractiveObjectAI):
+class LuigisCasaDellaTiresAI(InteractiveObjectAI):
     def __init__(self, air) -> None:
         # HACK: Renaming our class name here because
         # DistributedObjectAI will search dclassesByName
-        # for the non-existant DocsClinicAI dclass.
+        # for the non-existant LuigisCasaDellaTiresAI dclass.
         self.__class__.__name__ = "InteractiveObjectAI"
 
         InteractiveObjectAI.__init__(self, air)
 
         self.assetId = 5000
-        self.catalogId = 103
-        self.name = "landmark_docsclinic"
-        self.minigameId = 106
+        self.catalogId = 108
+        self.name = "landmark_luigitoss"
+        self.minigameId = 107
 
         # NOTE: This does not work so we use the `default_npc` script.
         # self.clientScript = "scripts/interactive/minigame_entry.lua"
