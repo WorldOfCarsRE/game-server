e60a06f79bf3f23b09807b18bf9eb0bd1f407ae0
Rocket <me@rocketprogrammer.me>
2025-02-17 18:49:05 -0600

    cleanup: `DistributedCarPuppetAI`

--- game/cars/carplayer/DistributedCarPuppetAI.py
+++ game/cars/carplayer/DistributedCarPuppetAI.py
@@ -20,44 +20,26 @@ class DistributedCarPuppetAI(DistributedCarGMAI):
     def __init__(self, air):
         DistributedCarGMAI.__init__(self, air)
 
-    def verifyAccess(self, avatarId: int) -> bool:
-        # We use this function to verify that we actally have access to GM functionality.
-        # Someone might manage to generate the GM object wiht a normal account somehow, should be impossible, but it could always happen somehow, (CarsClient error or something)
-        # TODO: Account access levels implementation?
-        avatar: self = self.air.getDo(avatarId)
-
-        if self.air.isProdServer() and avatar.getDISLid() not in self.air.staffMembers:
-            dg = PyDatagram()
-            dg.addServerHeader(self.GetPuppetConnectionChannel(avatarId), 0, AIMsgTypes.CLIENTAGENT_EJECT)
-            dg.addUint16(126)
-            dg.addString("You are not authorized to use administrator privileges.")
-            self.air.send(dg)
-            return False
-
-        return True
-
     def warnPlayer(self, playerId: int, context: int) -> None:
-        if self.verifyAccess(self.air.getAvatarIdFromSender()):
-            # Prepare the client message.
-            clientMsg = PyDatagram()
-            clientMsg.addUint16(CLIENT_SYSTEM_ALERT_WITHRESP)
-            clientMsg.addString("You have been warned by a staff member.")
-            self.sendMessage(playerId, clientMsg)
+        # Prepare the client message.
+        clientMsg = PyDatagram()
+        clientMsg.addUint16(CLIENT_SYSTEM_ALERT_WITHRESP)
+        clientMsg.addString("You have been warned by a staff member.")
+        self.sendMessage(playerId, clientMsg)
 
     def banPlayer(self, playerId: int, context: int) -> None:
-        if self.verifyAccess(self.air.getAvatarIdFromSender()):
-            # Prepare the client message.
-            clientMsg = PyDatagram()
-            clientMsg.addUint16(CLIENT_GO_GET_LOST_RESP)
-            clientMsg.addUint16(LOGOUT_REASON_ACCOUNT_DISABLED)
-            clientMsg.addString("There has been a reported violation of our Terms of Use connected to this account. For safety purposes, we have placed a temporary hold on the account.  For more details, please review the messages sent to the email address associated with this account.")
-            self.sendMessage(playerId, clientMsg)
+        # Prepare the client message.
+        clientMsg = PyDatagram()
+        clientMsg.addUint16(CLIENT_GO_GET_LOST_RESP)
+        clientMsg.addUint16(LOGOUT_REASON_ACCOUNT_DISABLED)
+        clientMsg.addString("There has been a reported violation of our Terms of Use connected to this account. For safety purposes, we have placed a temporary hold on the account.  For more details, please review the messages sent to the email address associated with this account.")
+        self.sendMessage(playerId, clientMsg)
 
-            player: DistributedCarPlayerAI = self.air.getDo(playerId)
+        player: DistributedCarPlayerAI | None = self.air.getDo(playerId)
 
-            if self.air.isProdServer() and player:
-                # Ban the player.
-                self.banAccount(player.getDISLname(), "Breaking rules.")
+        if self.air.isProdServer() and player:
+            # Ban the player.
+            self.banAccount(player.getDISLname(), "Breaking rules.")
 
     def sendMessage(self, playerId: int, clientMsg: PyDatagram) -> None:
         # Send it.
