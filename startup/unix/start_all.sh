70bdc9ad7f5efe989fa4ebb043a499f93c87731c
Jaden <me@rocketprogrammer.me>
2025-05-12 11:26:18 -0500

    add ud to startup unix

--- startup/unix/start_all.sh
+++ startup/unix/start_all.sh
@@ -3,4 +3,5 @@ cd ../../config
 screen -dmS OTP "../../OtpGo/otpgo" otp.yml
 
 cd ..
+screen -dmS UberDOG python3 -m game.cars.uberdog.Start config/config.prc
 screen -dmS Districts python3 -m DistrictStarter
