4c4b7bd5765172d7ec2500538218db679a089bc6
MatyCoder <matigamingproyt@gmail.com>
2025-05-19 05:28:05 -0400

    redhood-valley-ai: Improve the `Downtown Radiator Springs` hotspot

--- game/cars/carplayer/zones/RedhoodValleyAI.py
+++ game/cars/carplayer/zones/RedhoodValleyAI.py
@@ -2,7 +2,6 @@ from game.cars.carplayer.InteractiveObjectAI import (
     CMD_TYPE_POSITIVE, COMMAND_OFFER_QUERY_INTERACTIONS,
     COMMAND_SET_MAP_EFFECT, InteractiveObjectAI)
 
-
 class RedhoodValleyAI(InteractiveObjectAI):
     def __init__(self, air) -> None:
         # HACK: Renaming our class name here because
@@ -13,9 +12,9 @@ class RedhoodValleyAI(InteractiveObjectAI):
         InteractiveObjectAI.__init__(self, air)
 
         self.assetId = 5000
-        self.catalogId = 102
         self.name = "zone_redhoodValley"
+        self.clientScript = "scripts/interactive/teleporter.lua"
 
     def handleInteraction(self, avatarId: int, eventId: int, args: list) -> None:
         if eventId == COMMAND_OFFER_QUERY_INTERACTIONS:
-            self.d_setInteractiveCommands(avatarId, eventId, [COMMAND_SET_MAP_EFFECT, self.getCatalogId(), CMD_TYPE_POSITIVE])
+            self.setState(15003) # Redhood Valley zoneId
