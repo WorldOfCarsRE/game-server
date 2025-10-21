927cee9e7abe1ad04314767a8b422a28df01fd7c
MatyCoder <matigamingproyt@gmail.com>
2025-10-21 00:40:27 -0300

    Add GPS icon to `Redhood Valley` hotspot

--- game/cars/carplayer/zones/RedhoodValleyAI.py
+++ game/cars/carplayer/zones/RedhoodValleyAI.py
@@ -1,6 +1,6 @@
 from game.cars.carplayer.InteractiveObjectAI import (
     CMD_TYPE_POSITIVE, COMMAND_OFFER_QUERY_INTERACTIONS,
-    COMMAND_SET_MAP_EFFECT, InteractiveObjectAI)
+    COMMAND_SHOW_GPS, InteractiveObjectAI)
 
 class RedhoodValleyAI(InteractiveObjectAI):
     def __init__(self, air) -> None:
@@ -12,9 +12,11 @@ class RedhoodValleyAI(InteractiveObjectAI):
         InteractiveObjectAI.__init__(self, air)
 
         self.assetId = 5000
+        self.catalogId = 111 # GPS icon
         self.name = "zone_redhoodValley"
         self.clientScript = "scripts/interactive/teleporter.lua"
 
     def handleInteraction(self, avatarId: int, eventId: int, args: list) -> None:
         if eventId == COMMAND_OFFER_QUERY_INTERACTIONS:
             self.setState(15003) # Redhood Valley zoneId
+            self.d_setInteractiveCommands(avatarId, eventId, [COMMAND_SHOW_GPS, self.getCatalogId(), CMD_TYPE_POSITIVE])
